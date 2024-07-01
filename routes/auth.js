1b3453bfae11e8974fd2a96e98c52e0507658f82
Besslan <36267456+Besslan@users.noreply.github.com>
2024-07-01 10:38:57 +0200

    remove unused code

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { register, login, getProfile } from "../controllers/users.js";
+import { login, getProfile } from "../controllers/users.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.post("/register", register);
 router.get("/profile", authenticate, getProfile);
 
 export default router;
