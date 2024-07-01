964221a0691247645af864a9dd208d7ae8fe4c4d
Besslan <36267456+Besslan@users.noreply.github.com>
2024-07-01 11:17:18 +0200

    add request validation on login and register routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { register } from "../controllers/users.js";
+import { validateUser } from "../middlewares/validateRequest.js";
 
 const router = express.Router();
 
-router.post("/", register);
+router.post("/", validateUser, register);
 
 export default router;
