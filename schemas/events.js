9f9f5275db71232a734c5fc14e338f8848f6c021
Besslan <36267456+Besslan@users.noreply.github.com>
2024-07-06 16:23:25 +0200

    update swagger schema and response

--- schemas/events.js
+++ schemas/events.js
@@ -5,7 +5,7 @@ export const eventSchema = {
     title: Joi.string().min(3).max(255).required(),
     description: Joi.string().max(5000).optional(),
     date: Joi.date().iso().required(),
-    location: Joi.string().max(255).optional(),
+    location: Joi.string().max(255).required(),
     latitude: Joi.number().precision(8).optional().allow(null),
     longitude: Joi.number().precision(8).optional().allow(null),
     organizerId: Joi.number().integer().required(),
